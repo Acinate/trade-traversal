cab23424806f4db20ba06a9ef6c619ba0f36b22d
Jason <acinate@hotmail.com>
2020-08-09 22:50:14 -0400

    fixed redirect error

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,20 +1,20 @@
-import express, {Request, Response} from 'express';
-import path from 'path';
-import helmet from 'helmet';
-import morgan from 'morgan';
-import webpack from 'webpack';
-import webpackDevMiddleware from 'webpack-dev-middleware';
-import webpackHotMiddleware from 'webpack-hot-middleware';
-import {ENVIRONMENT, PORT} from '../util/secrets';
+import express, {Request, Response} from "express";
+import path from "path";
+import helmet from "helmet";
+import morgan from "morgan";
+import webpack from "webpack";
+import webpackDevMiddleware from "webpack-dev-middleware";
+import webpackHotMiddleware from "webpack-hot-middleware";
+import {ENVIRONMENT, PORT} from "../util/secrets";
 // Import API Routes
-import * as home from './controllers/home';
+import * as home from "./controllers/home";
 
 // Create express server
 const app = express();
 
 // Add middleware
 app.use(helmet());
-app.use(morgan('combined'));
+app.use(morgan("combined"));
 
 // Define API routes
 app.get('/api/', home.get);
@@ -22,14 +22,15 @@ app.get('/api/', home.get);
 // Configure environment settings
 if (ENVIRONMENT === 'development') {
     // Configure Webpack Dev Server (with React Hot-Reload)
-    const webpackConfig = require('../../webpack.dev.js');
+    const webpackConfig = require("../../webpack.dev.js");
     const compiler = webpack(webpackConfig);
-
+    const history = require("connect-history-api-fallback");
+    app.use(history());
     app.use(
-        webpackDevMiddleware(compiler, {
-            publicPath: "/",
-            stats: "errors-warnings"
-        })
+      webpackDevMiddleware(compiler, {
+          publicPath: "/",
+          stats: "errors-warnings"
+      })
     );
     app.use(webpackHotMiddleware(compiler));
 } else {
